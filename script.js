d8837e8b508c4f8910c7e1609e095707b657a882
kevinluna94 <kevinemanuelluna@gmail.com>
2025-08-11 19:52:22 -0300

    cambios en la estructura inscripciones y descargas, footer y navbar

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 // Aquí puedes añadir funciones extra si necesitas interactividad personalizada
 console.log("Abierto Pampeano cargado correctamente.");
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const navbarCollapse = document.getElementById('navbarNav');
+    const bsCollapse = new bootstrap.Collapse(navbarCollapse, { toggle: false });
+
+    document.querySelectorAll('#navbarNav .nav-link').forEach(function (navLink) {
+      navLink.addEventListener('click', function () {
+        if (navbarCollapse.classList.contains('show')) {
+          bsCollapse.hide();
+        }
+      });
+    });
+  });
+
